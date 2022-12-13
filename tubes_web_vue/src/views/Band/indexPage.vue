49606928363d2e888e6b8427f9f49f5e35379c89
ImmanuelArvellian <arvellianputra@gmail.com>
2022-12-13 22:13:01 +0700

    index page tiap event

--- tubes_web_vue/src/views/Band/indexPage.vue
+++ tubes_web_vue/src/views/Band/indexPage.vue
@@ -1,56 +1,85 @@
 <template>
     <body>
-        <div class="banner text">
-            <div class="banner-body">
-                <div class="wrapper">
-                    <div class="content">
-                        <div class="text-center">
-                            <h2>Our Best Band List</h2>
-                            <h4>
-                                AMP Group always ensures events with guaranteed quality 
-                                and you are sure to get the best quality events in Indonesia. 
-                                We also have a wide selection of events available to suit your needs. 
-                                So, you can choose an event with the needs you want.
-                            </h4>
-
-                            <!-- <div class="container px-4 text-center"> -->
-                                <div class="row gx-5">
-                                    <div class="col">
-                                        <div class="card text-center" style="width: 18rem;">
-                                            <img src="/src/assets/logo.png" class="card-img-top" alt="...">
-                                            <div class="card-body overflow-hidden">
-                                                <h5 class="card-title">Band 1</h5>
-                                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                                <router-link :to="{name: 'detailBand'}" class="btn btn-primary">See Profile</router-link>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col">
-                                        <div class="card text-center" style="width: 18rem;">
-                                            <img src="/src/assets/logo.png" class="card-img-top" alt="...">
-                                            <div class="card-body overflow-hidden">
-                                                <h5 class="card-title">Band 2</h5>
-                                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                                <router-link :to="{name: 'detailBand'}" class="btn btn-primary">See Profile</router-link>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col">
-                                        <div class="card text-center" style="width: 18rem;">
-                                            <img src="/src/assets/logo.png" class="card-img-top" alt="...">
-                                            <div class="card-body overflow-hidden">
-                                                <h5 class="card-title">Band</h5>
-                                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                                <router-link :to="{name: 'detailBand'}" class="btn btn-primary">See Profile</router-link>
-                                            </div>
-                                        </div>
-                                    </div>
+        <!-- List Event -->
+    <div class="p-5"></div>
+    <div class="container-fluid bg-light">
+        <div class="row gx-4 gx-lg-5 h-100 align-items-center justify-content-center text-center">
+            <div class="col-lg-8 align-self-end">
+                <h1 class="text-dark font-weight-bold">List Band Terbaik Kami</h1>
+            </div>
+            <div class="col-lg-10 align-self-baseline">
+                <p class="text-white-75 mb-1">Akordmusic Production selalu memastikan band dengan kualitas terjamin dan
+                    Anda
+                    pasti mendapatkan kualitas band terbaik di Bali. Kita juga memiliki banyak pilihan band yang
+                    tersedia
+                    dengan gaya band nya masing-masing sesuai kebutuhan Anda. Jadi, Anda bisa memilih band dengan
+                    kebutuhan
+                    yang Anda inginkan.</p>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="card mb-4 box-shadow">
+                            <img class="card-img-top"
+                                src="https://cdn.antaranews.com/cache/730x487/2022/09/30/KOTAK_0316.jpg" alt="...">
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    <h5 class="fw-bolder">Band 1</h5>
+                                    Akordkustik ipsum dolor sit amet, consectetur adipiscing elit. Semper duis tellus
+                                    suspendisse
+                                    felis, odio vitae ultrices quam placerat. Est nisl massa placerat facilisis etiam
+                                    vulputate
                                 </div>
-                            <!-- </div> -->
+                            </div>
+                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">More</a></div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4">
+                        <div class="card mb-4 box-shadow">
+                            <img class="card-img-top"
+                                src="https://cdn.antaranews.com/cache/730x487/2020/10/02/e9018782-0ef5-446e-83d0-3db221524a96.jpg"
+                                alt="...">
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    <h5 class="fw-bolder">Band 2</h5>
+                                    Akordkustik ipsum dolor sit amet, consectetur adipiscing elit. Semper duis tellus
+                                    suspendisse
+                                    felis, odio vitae ultrices quam placerat. Est nisl massa placerat facilisis etiam
+                                    vulputate
+                                </div>
+                            </div>
+                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">More</a></div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4">
+                        <div class="card mb-4 box-shadow">
+                            <img class="card-img-top"
+                                src="https://telltaletv.com/wp-content/uploads/2018/01/The-Magicians-Season-3-Episode-3_NUP_179567_0093-730x487.jpg"
+                                alt="...">
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    <h5 class="fw-bolder">Band 3</h5>
+                                    Akordkustik ipsum dolor sit amet, consectetur adipiscing elit. Semper duis tellus
+                                    suspendisse
+                                    felis, odio vitae ultrices quam placerat. Est nisl massa placerat facilisis etiam
+                                    vulputate
+                                </div>
+                            </div>
+                            <!-- Product actions-->
+                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">More</a></div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
     </body>
 </template>
